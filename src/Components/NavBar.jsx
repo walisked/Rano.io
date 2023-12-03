c8101903a8cc37c33ca8d5615822d71ce75c4d0a
walisked <umararano@gmail.com>
2023-12-03 23:03:03 +0100

    pgcomponet

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//add Link Route
+
+export const NavBar = () => {
+  return (
+        <> 
+        <nav>
+            <ul>
+                <li> HOME </li>
+                <li> ABOUT </li>
+                <li> SKILLS </li>
+            </ul>
+        </nav>
+        </>
+  )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavBar } from '../Components/NavBar'
+import { Hero } from '../Components/Hero'
+import { Footer } from '../Components/Footer'
+
+export const Home = () => {
+  return (
+   <>
+        <NavBar/>
+        <Hero/>
+        <Footer/>
+   </>
+  )
+}
