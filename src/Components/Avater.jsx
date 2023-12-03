a544a8e7b0bc28f8922e2b26cf646839bb5ae69a
walisked <umararano@gmail.com>
2023-12-03 23:25:14 +0100

    routercode

---
+++ src/Components/Avater.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Avater = () => {
+  return (
+    <div>
+        <img class="rounded w-36 h-36" src="/docs/images/people/profile-picture-5.jpg" alt="Extra large avatar"></img>
+    </div>
+  )
+}
