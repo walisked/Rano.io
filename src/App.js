1c3489d25612df7a3c0c14d33aa6167572f20a1c
walisked <umararano@gmail.com>
2023-12-11 12:38:01 +0100

    profile into

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter,Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
-import Profile from "./Pages/Profile";
+import { Profile } from "./Pages/Profile";
 
 
 function App() {
